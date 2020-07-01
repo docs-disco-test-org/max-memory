ff983bcdb69227bf77816fa98a3244ab42fd4cb9
Roy van de Water <roy.vwater@gmail.com>
2020-07-01 10:26:51 -0700

    Trying again

--- main.js
+++ main.js
@@ -3,10 +3,17 @@
 var uuid = '00000000-0000-0000-0000-000000000000'
 
 var main = () => {
-  var memory = '';
+  var memory = [];
+  var string = '';
+  var count = 0;
+
+  for (var i = 0; i < 100 * 100; i++) {
+    string += uuid;
+  }
 
   while (true) {
-    memory += uuid
+    memory.push(string + count);
+    count += 1;
   }
 }
 main()
